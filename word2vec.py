898a1ea2b76e9653cd91495a5f8b4683a7a81e6a
kent0304 <tanaken0304.0123@gmail.com>
2020-11-20 21:26:39 +0900

    Finish creating word2vec model in recipe data

--- word2vec.py
+++ word2vec.py
@@ -1,19 +1,41 @@
 import re
 import MeCab
+from gensim.models import word2vec
 
+# 形態素解析
+def tokenize(sentence):
+    mecab = MeCab.Tagger('-Owakati')
+    text = mecab.parse(sentence)
+    return [text.strip()]
 
 
 # ファイル読み込み
 with open('data/text/train.txt') as f:
     train_text = [line.strip() for line in f.readlines()]
-    train_text = [re.sub('\t', '', line) for line in train_text]
+    train_text = '\t'.join(train_text)
+    train_text = train_text.split('\t')
 with open('data/text/valid.txt') as f:
     valid_text = [line.strip() for line in f.readlines()]
-    valid_text = [re.sub('\t', '', line) for line in valid_text]
+    valid_text = '\t'.join(valid_text)
+    valid_text = valid_text.split('\t')
 with open('data/text/test.txt') as f:
     test_text = [line.strip() for line in f.readlines()]
-    test_text = [re.sub('\t', '', line) for line in test_text]
+    test_text = '\t'.join(test_text)
+    test_text = test_text.split('\t')
 
+# レシピコーパス  
 text = train_text + valid_text + test_text
 
-me = MeCab.Tagger()
+# 二次元配列で形態素を管理
+token_data = []
+for sentence in text:
+    token_data.append(tokenize(sentence))
+
+model = word2vec.Word2Vec(sentences=token_data,
+                             vector_size=300,
+                             window=5,
+                             min_count=1,
+                             workers=8,
+                             epochs=100)
+
+model.save("data/word2vec.model")
