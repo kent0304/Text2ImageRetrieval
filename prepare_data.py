967589d1d545f8f2ee3418062da67be8336be20a
kent0304 <tanaken0304.0123@gmail.com>
2020-12-04 10:30:36 +0900

    Move image2vec function to getitem in MyDataset class

--- prepare_data.py
+++ prepare_data.py
@@ -13,20 +13,20 @@ import random
 from natsort import natsorted
 from PIL import Image
 from multiprocessing import Pool
-from torchvision import transforms
-from torch.utils.data import Dataset, DataLoader
-from model import OriginalResNet, TripletModel
+from torchvision import transforms, models
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+from model import TripletModel
 from gensim.models import Word2Vec
 
-# special_dataset = [
-#     ['ルッコラは洗って水気を拭き取る。 豚バラは一口大にカットしてみじん切りにしたらパセリ、塩コショウで下味をつける。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_1.jpg'],
-#     ['パスタを茹で始める。8分', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_2.jpg'],
-#     ['にんにくを温めたら豚バラを炒める。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_3.jpg'],
-#     ['豚バラの色が変わったらルッコラを入れて軽く火を通す。 トマト缶を投入。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_4.jpg'],
-#     ['しばらくコトコト。パスタの茹で汁と塩で味を調整。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_5.jpg'],
-#     ['茹で上がったパスタを投入。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_6.jpg'],
-#     ['盛りつけたら完成。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_7.jpg']
-# ]
+special_dataset = [
+    ['ルッコラは洗って水気を拭き取る。 豚バラは一口大にカットしてみじん切りにしたらパセリ、塩コショウで下味をつける。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_1.jpg'],
+    ['パスタを茹で始める。8分', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_2.jpg'],
+    ['にんにくを温めたら豚バラを炒める。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_3.jpg'],
+    ['豚バラの色が変わったらルッコラを入れて軽く火を通す。 トマト缶を投入。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_4.jpg'],
+    ['しばらくコトコト。パスタの茹で汁と塩で味を調整。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_5.jpg'],
+    ['茹で上がったパスタを投入。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_6.jpg'],
+    ['盛りつけたら完成。', '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/0/0c6aad2feba89eb88219e69c2b9b15c8a1d62045_7.jpg']
+]
 
 
 # レシピコーパスで学習したWord2Vec
@@ -35,8 +35,6 @@ model = Word2Vec.load("/mnt/LSTA5/data/tanaka/retrieval/text2image/word2vec.mode
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # 損失関数
 triplet_loss = nn.TripletMarginLoss()
-# netにはモデルを代入
-image_net = OriginalResNet()
 # 学習させるモデル
 triplet_model = TripletModel()
 # 画像を Tensor に変換
@@ -47,43 +45,6 @@ transformer = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     # transforms.RandomCrop(256),
 ])
-
-class MyDataset(Dataset):
-    def __init__(self, dataset, text_model=model, transformer=transformer):
-        self.data_num = len(dataset)
-        self.sentence_vec = []
-        self.image_vec = []
-        image_net.eval()
-        for text, image_path in tqdm(dataset, total=self.data_num):
-            if text=='' or image_path=='':
-                continue
-            # 形態素解析
-            mecab = MeCab.Tagger("-Owakati")
-            token_list = mecab.parse(text).split()
-            # 文全体をベクトル化
-            sentence_sum = np.zeros(text_model.wv.vectors.shape[1], )
-            for token in token_list:
-                if token in text_model.wv:
-                    sentence_sum += text_model.wv[token]
-                else:
-                    continue
-            sentence = sentence_sum / len(token_list)
-            sentence = torch.from_numpy(sentence).clone()
-            self.sentence_vec.append(sentence)
-
-            # 画像のベクトル化
-            image = transformer(Image.open(image_path).convert('RGB')).unsqueeze(0)
-            image = image_net(image)
-            # image = torch.from_numpy(image).clone()
-            self.image_vec.append(image)
-    
-    def __len__(self):
-        return self.data_num
-
-    def __getitem__(self, idx):
-        sentence_vec = self.sentence_vec[idx]
-        image_vec = self.image_vec[idx]
-        return sentence_vec, image_vec
 
 
 # ファイルの読み込み
@@ -148,8 +109,6 @@ def make_dataset(recipe_data):
             continue
         # テキストファイル読み込み
         recipe_text = read_file(recipe_dir)
-
-
         # 画像
         head = recipe[0]
         path = '/mnt/LSTA5/data/common/recipe/cookpad_image_dataset/images/steps/' + head + '/' + recipe + '*'
@@ -157,27 +116,76 @@ def make_dataset(recipe_data):
         img_paths = natsorted(glob.glob(path))
         if len(recipe_text) != len(img_paths):
             continue
-            # print('あかん')
-            # print('ハッシュ値：', recipe)
-            # print('テキストのパスは')
-            # print(recipe_dir)
-            # print('テキスト数は')
-            # print(len(recipe_text))
-            # print('テキストは')
-            # print(recipe_text)
-            # print('画像のパスは')
-            # print(img_paths)
 
         # 画像は余分にあるのでテキスト基準で
         for i in range(len(recipe_text)):
             dataset.append([recipe_text[i], img_paths[i]])
 
     return dataset
+
+class MyDataset(Dataset):
+    def __init__(self, dataset, text_model=model, transformer=transformer):
+        self.data_num = len(dataset)
+        self.sentence_vec = []
+        self.image_paths = []
+        for text, image_path in tqdm(dataset, total=self.data_num):
+            if text=='' or image_path=='':
+                continue
+            # 形態素解析
+            mecab = MeCab.Tagger("-Owakati")
+            token_list = mecab.parse(text).split()
+            # 文全体をベクトル化
+            sentence_sum = np.zeros(text_model.wv.vectors.shape[1], )
+            for token in token_list:
+                if token in text_model.wv:
+                    sentence_sum += text_model.wv[token]
+                else:
+                    continue
+            sentence = sentence_sum / len(token_list)
+            sentence = torch.from_numpy(sentence).clone()
+            self.sentence_vec.append(sentence)
+            # 画像
+            # ここではパスだけ追加
+            self.image_paths.append(image_path)
+        
+    def __len__(self):
+        return self.data_num
+
+    def __getitem__(self, idx):
+        sentence_vec = self.sentence_vec[idx]
+        image_path = self.image_paths[idx]
+        # 画像のベクトル化
+        image_vec = image2vec(image_path)
+        return sentence_vec, image_vec
+
+def image2vec(image_path):
+    # ブログ参考
+    image_net = models.resnet50(pretrained=True)
+    image_net.fc = nn.Identity()
+
+    # 画像を Tensor に変換
+    transformer = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    # 画像のベクトル化
+    image_net.eval()
+    image_net = image_net.to(device)
+    image = transformer(Image.open(image_path).convert('RGB')).unsqueeze(0)
+    image = image.to(device)
+    image = image_net(image)
+    return image.to('cpu').flatten()
+
+
+
+
  
-print('train/valid/testに分けられたハッシュ列取得...')
-recipe_train = get_hash('train')
-recipe_valid = get_hash('val')
-recipe_test = get_hash('test')
+# print('train/valid/testに分けられたハッシュ列取得...')
+# recipe_train = get_hash('train')
+# recipe_valid = get_hash('val')
+# recipe_test = get_hash('test')
 
 # print('データセットから二次元配列に生テキストと画像パス保管中...')
 # train_dataset = make_dataset(recipe_train)
@@ -206,12 +214,15 @@ print('データセットをDatasetに変換中...')
 train_dataset = MyDataset(train_dataset)
 valid_dataset = MyDataset(valid_dataset)
 test_dataset = MyDataset(test_dataset)
-print(test_dataset[0])
-print(type(test_dataset[0][0]))
-print(type(test_dataset[0][1]))
-# special_dataset = MyDataset(special_dataset)
+special_dataset = MyDataset(special_dataset)
+# print(special_dataset[0])
+# print(special_dataset[0][0].shape)
+# print(special_dataset[0][1].shape)
 
-# pickleで保存
+
+
+
+# # pickleで保存
 print('pickleで保存')
 with open('/mnt/LSTA5/data/tanaka/retrieval/text2image/torch_dataset/train_dataset.pkl', 'wb') as f:
     pickle.dump(train_dataset, f) 
@@ -219,3 +230,5 @@ with open('/mnt/LSTA5/data/tanaka/retrieval/text2image/torch_dataset/valid_datas
     pickle.dump(valid_dataset, f) 
 with open('/mnt/LSTA5/data/tanaka/retrieval/text2image/torch_dataset/test_dataset.pkl', 'wb') as f:
     pickle.dump(test_dataset, f) 
+# with open('/mnt/LSTA5/data/tanaka/retrieval/text2image/torch_dataset/special_dataset.pkl', 'wb') as f:
+#     pickle.dump(special_dataset, f) 
