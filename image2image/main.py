65316e329b31f1fb5f6a6b2a2f7be2822cfd9d19
kent0304 <tanaken0304.0123@gmail.com>
2020-12-05 16:05:37 +0900

    Add cosine similarity by numpy as a function

--- image2image/main.py
+++ image2image/main.py
@@ -5,7 +5,7 @@ import random
 from PIL import Image
 from torch import nn, optim
 from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
+from torchvision import transforms, models
 from tqdm import tqdm
 from scipy import spatial
 
@@ -23,24 +23,27 @@ def prepare_recipes():
         image_data.append(test_dataset[idx][1])
     return image_data
 
-# 画像間の類似度
-def image_similarity(img1_vec, img2_vec):
-    return 1 - spatial.distance.cosine(img1_vec, img2_vec)
+
+def image_similarity(v1, v2):
+    return  np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
 
 def image2vec(image_path):
-    image_net = OriginalResNet()
+    # ブログ参考
+    image_net = models.resnet50(pretrained=True)
+    image_net.fc = nn.Identity()
+
     # 画像を Tensor に変換
     transformer = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        # transforms.RandomCrop(256),
     ])
     # 画像のベクトル化
+    image_net.eval()
     image = transformer(Image.open(image_path).convert('RGB')).unsqueeze(0)
-    image = image_net(image).numpy()
-    return image
+    image = image_net(image).detach().numpy().copy()
+    return image.flatten()
 
 def measurement(target, image_data):
     # 辞書の値からキー抽出
@@ -69,17 +72,17 @@ def measurement(target, image_data):
 
 def main():
     image_data = prepare_recipes()
-   
-    similar_dict = measurement(image_data[0], image_data)
-    similar_dict2 = measurement(image_data[1], image_data)
 
-    print(f'{image_data[0]}に近い画像top10')
-    for k, v in similar_dict.items():
-        print(k, v)
+    print(image_similarity(image2vec('/mnt/LSTA5/data/sakoda/image_step_dataset/3ae13f68982975988da11927410c4465b3104bf2/3ae13f68982975988da11927410c4465b3104bf2_9.jpg'),  image2vec('/mnt/LSTA5/data/sakoda/image_step_dataset/45f31a2c32e015d814365ae66a3a77d5a38c8145/45f31a2c32e015d814365ae66a3a77d5a38c8145_5.jpg')))
+   
+    similar_dict = measurement(image_data[100], image_data)
+    print(f'{image_data[100]}に近い画像top10')
+    for _ in similar_dict:
+        print(_)
+
     print('')
-    print(f'{image_data[1]}に近い画像top10')
-    for k, v in similar_dict2.items():
-        print(k, v)
+    dog = '/home/tanaka/projects/retrieval/dog.jpg'
+    print(image_similarity(image2vec(image_data[100]), image2vec(dog)))
 
 
 if __name__ == '__main__':
