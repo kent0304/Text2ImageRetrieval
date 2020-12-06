314bb2fc3aa561dd4baedfa67226461eba71bb7d
kent0304 <tanaken0304.0123@gmail.com>
2020-12-06 16:22:07 +0900

    Make some models for word vectors input

--- model.py
+++ model.py
@@ -18,17 +18,31 @@ class TripletModel(nn.Module):
         self.image_fc1 = nn.Linear(2048, 1536)
         self.image_fc2 = nn.Linear(1536, 1024)
         self.image_fc3 = nn.Linear(1024, 512)
-        self.text_fc1 = nn.Linear(300, 400)
-        self.text_fc2 = nn.Linear(400, 450)
-        self.text_fc3 = nn.Linear(450, 512)
+        # self.text_fc1 = nn.Linear(100, 256)
+        # self.text_fc2 = nn.Linear(256, 400)
+        # self.text_fc3 = nn.Linear(400, 512)
+        # self.text_fc1 = nn.Linear(200, 256)
+        # self.text_fc2 = nn.Linear(256, 400)
+        # self.text_fc3 = nn.Linear(400, 512)
+        self.text_fc1 = nn.Linear(300, 372)
+        self.text_fc2 = nn.Linear(372, 446)
+        self.text_fc3 = nn.Linear(446, 512)
 
-    def forward(self, text, image):
-        text = F.relu(self.text_fc1(text))
-        text = F.relu(self.text_fc2(text))
-        text_vec = self.text_fc3(text)
+    def forward(self, pos_text, pos_image, neg_text, neg_image):
+        pos_text = F.relu(self.text_fc1(pos_text))
+        pos_text = F.relu(self.text_fc2(pos_text))
+        pos_text_vec = self.text_fc3(pos_text)
 
-        image = F.relu(self.image_fc1(image))
-        image = F.relu(self.image_fc2(image))
-        image_vec = self.image_fc3(image)
+        pos_image = F.relu(self.image_fc1(pos_image))
+        pos_image = F.relu(self.image_fc2(pos_image))
+        pos_image_vec = self.image_fc3(pos_image)
 
-        return text_vec, image_vec
+        neg_text = F.relu(self.text_fc1(neg_text))
+        neg_text = F.relu(self.text_fc2(neg_text))
+        neg_text_vec = self.text_fc3(neg_text)
+
+        neg_image = F.relu(self.image_fc1(neg_image))
+        neg_image = F.relu(self.image_fc2(neg_image))
+        neg_image_vec = self.image_fc3(neg_image)
+
+        return pos_text_vec, pos_image_vec, neg_text_vec, neg_image_vec
